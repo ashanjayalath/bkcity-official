eb269dd0059d3e2b0d87cbb135b8dd5cc6587596
ashanjayalath <shanjayalath225@gmail.com>
2023-02-09 00:35:13 +0530

    change animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,28 @@ import {onAuthStateChanged } from "firebase/auth";
 import Router from "next/router";
 import {FCRUD} from '../components/firebase/firebaseDatabaseConnector'
 import BoxCircle from "../components/animations/boxCircle";
+import {useEffect} from "react";
 
 export default function Home() {
 
     const auth = FCRUD.auth
     const user = auth.currentUser;
 
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in
-            Router.push('/home')
-        } else {
-            // User is signed out
-            Router.push('/sign')
-        }
-    });
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in
+                Router.push('/home')
+            } else {
+                // User is signed out
+                Router.push('/sign')
+            }
+        });
+    },[])
+
     return<>
         <Row>
-            <Col span={4} offset={10} style={{textAlign:"center",marginTop:200}}>
+            <Col span={2} offset={12} style={{textAlign:"center",marginTop:200}}>
                 {/*<Spin spinning/>*/}
                 <BoxCircle/>
             </Col>
