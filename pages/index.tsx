2bcb33cf0b0c1c9bb152d19366015ef1f7635b4c
ashanjayalath <shanjayalath225@gmail.com>
2023-02-09 00:39:28 +0530

    add async

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,23 @@ import {FCRUD} from '../components/firebase/firebaseDatabaseConnector'
 import BoxCircle from "../components/animations/boxCircle";
 import {useEffect} from "react";
 
-export default function Home() {
+export default async function Home() {
 
     const auth = FCRUD.auth
     const user = auth.currentUser;
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in
-                Router.push('/home')
-            } else {
-                // User is signed out
-                Router.push('/sign')
-            }
-        });
-    },[])
-
-    return<>
+    await onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in
+            Router.push('/home')
+        } else {
+            // User is signed out
+            Router.push('/sign')
+        }
+    });
+    return <>
         <Row>
-            <Col span={2} offset={12} style={{textAlign:"center",marginTop:200}}>
+            <Col span={2} offset={12} style={{textAlign: "center", marginTop: 200}}>
                 {/*<Spin spinning/>*/}
                 <BoxCircle/>
             </Col>
