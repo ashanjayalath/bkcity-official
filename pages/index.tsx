22bc957e4e42ed33d2154cb2d888195f27ee28bf
ashanjayalath <shanjayalath225@gmail.com>
2023-02-09 00:44:25 +0530

    change firebase auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,33 @@
-import {Col, message, Row, Spin} from "antd";
+import {Col, Row} from "antd";
 import {onAuthStateChanged } from "firebase/auth";
 import Router from "next/router";
 import {FCRUD} from '../components/firebase/firebaseDatabaseConnector'
 import BoxCircle from "../components/animations/boxCircle";
 import {useEffect} from "react";
 
-export default async function Home() {
+export default function Home() {
 
     const auth = FCRUD.auth
     const user = auth.currentUser;
 
-    await onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in
-            Router.push('/home')
-        } else {
-            // User is signed out
-            Router.push('/sign')
-        }
-    });
-    return <>
+    useEffect(()=>{
+       async()=>{
+          await onAuthStateChanged(auth, (user) => {
+               if (user) {
+                   // User is signed in
+                   Router.push('/home')
+               } else {
+                   // User is signed out
+                   Router.push('/sign')
+               }
+           });
+       }
+
+    },[auth])
+
+    return<>
         <Row>
-            <Col span={2} offset={12} style={{textAlign: "center", marginTop: 200}}>
+            <Col span={2} offset={12} style={{textAlign:"center",marginTop:200}}>
                 {/*<Spin spinning/>*/}
                 <BoxCircle/>
             </Col>
